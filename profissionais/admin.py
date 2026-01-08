bc43d82fbc247c8b2ec3e7cfda28132f9bcfe1a3
luanguedes <37886916+luanguedes@users.noreply.github.com>
2026-01-08 22:40:23 +0000

    profissionais e esp no admin

--- profissionais/admin.py
+++ profissionais/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profissional, Especialidade
 
-# Register your models here.
+@admin.register(Especialidade)
+class EspecialidadeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ('nome',)
+
+@admin.register(Profissional)
+class ProfissionalAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'crm', 'ativo') # Ajuste 'crm' ou 'registro' conforme seu model
+    search_fields = ('nome', 'crm')
+    
+    # Isso cria aquela caixa dupla para selecionar várias especialidades
+    # Se der erro dizendo que o campo não existe, verifique se no model chama 'especialidades' ou 'especialidade'
+    filter_horizontal = ('especialidades',)
