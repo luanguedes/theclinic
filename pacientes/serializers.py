883d1058e8d65cf67ff634c0f2815ffd8750fbcc
luanguedes <37886916+luanguedes@users.noreply.github.com>
2026-01-08 23:57:58 +0000

    ajuste serializers DateField

--- pacientes/serializers.py
+++ pacientes/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Paciente
 
 class PacienteSerializer(serializers.ModelSerializer):
+    # --- A CORREÇÃO ESTÁ AQUI EMBAIXO ---
+    # Declaramos explicitamente para garantir que o DRF trate como DateField simples
+    data_nascimento = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d", "%d/%m/%Y"])
+
     class Meta:
         model = Paciente
         fields = '__all__'
