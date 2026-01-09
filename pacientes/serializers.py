26d4a4634b38b1e12fb8f9c286ea89dc7318ea07
luanguedes <37886916+luanguedes@users.noreply.github.com>
2026-01-09 00:05:07 +0000

    ajuste pacientes

--- pacientes/serializers.py
+++ pacientes/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Paciente
 
 class PacienteSerializer(serializers.ModelSerializer):
-    # --- A CORREÇÃO ESTÁ AQUI EMBAIXO ---
-    # Declaramos explicitamente para garantir que o DRF trate como DateField simples
+    # --- A CORREÇÃO ESTÁ AQUI ---
+    # Forçamos o tipo DateField para ele não tentar calcular fuso horário
     data_nascimento = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d", "%d/%m/%Y"])
 
     class Meta:
