02126025e5e3a8ddaf370b284869b22ef5b348a2
luanguedes <37886916+luanguedes@users.noreply.github.com>
2026-01-10 17:12:21 +0000

    commit erro

--- configuracoes/views.py
+++ configuracoes/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions, filters, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import Convenio, DadosClinica
+from .models import Convenio, DadosClinica, ConfiguracaoSistema
 from .serializers import ConvenioSerializer, DadosClinicaSerializer, ConfiguracaoSistemaSerializer
 
 class ConvenioViewSet(viewsets.ModelViewSet):
