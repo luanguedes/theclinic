202b5bed8f508cdb88b1690ceb87a4e77b3bbd10
luanguedes <37886916+luanguedes@users.noreply.github.com>
2026-01-13 02:28:13 +0000

    ajuste imports serializer

--- profissionais/serializers.py
+++ profissionais/serializers.py
@@ -1,10 +1,57 @@
+from rest_framework import serializers
+
+from .models import (
+    Profissional,
+    Especialidade,
+    ProfissionalEspecialidade
+)
+
+
+class EspecialidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Especialidade
+        fields = ['id', 'nome']
+
+
+class ProfissionalEspecialidadeSerializer(serializers.ModelSerializer):
+    nome_especialidade = serializers.CharField(
+        source='especialidade.nome',
+        read_only=True
+    )
+
+    especialidade_id = serializers.PrimaryKeyRelatedField(
+        queryset=Especialidade.objects.all(),
+        source='especialidade',
+        write_only=True
+    )
+
+    especialidade_leitura = serializers.PrimaryKeyRelatedField(
+        source='especialidade',
+        read_only=True
+    )
+
+    class Meta:
+        model = ProfissionalEspecialidade
+        fields = [
+            'id',  # ID do vínculo, NÃO da especialidade
+            'especialidade_id',
+            'especialidade_leitura',
+            'nome_especialidade',
+            'sigla_conselho',
+            'registro_conselho',
+            'uf_conselho'
+        ]
+
+
 class ProfissionalSerializer(serializers.ModelSerializer):
+    # Estrutura completa para telas administrativas
     especialidades = ProfissionalEspecialidadeSerializer(
         source='especialidades_vinculo',
         many=True,
         read_only=True
     )
 
+    # Estrutura simplificada para a recepção (dropdown)
     especialidades_lista = serializers.SerializerMethodField()
 
     class Meta:
@@ -19,6 +66,10 @@ class ProfissionalSerializer(serializers.ModelSerializer):
         ]
 
     def get_especialidades_lista(self, obj):
+        """
+        Retorna lista limpa de especialidades vinculadas ao profissional:
+        [{id, nome}]
+        """
         return [
             {
                 "id": vinculo.especialidade.id,
@@ -29,7 +80,10 @@ class ProfissionalSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        especialidades_data = self.context['request'].data.get('especialidades', [])
+        especialidades_data = self.context['request'].data.get(
+            'especialidades',
+            []
+        )
 
         profissional = Profissional.objects.create(**validated_data)
 
@@ -59,6 +113,7 @@ class ProfissionalSerializer(serializers.ModelSerializer):
 
         if especialidades_data is not None:
             instance.especialidades_vinculo.all().delete()
+
             for spec in especialidades_data:
                 especialidade_id = spec.get('especialidade_id')
                 if especialidade_id:
