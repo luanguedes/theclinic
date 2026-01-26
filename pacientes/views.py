5549c525877f95c27f63dfef3a519cb780bbcc85
luanguedes <luan.guedes@edu.unipar.br>
2026-01-26 00:38:33 -0300

    erro ver lista

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions, filters
+﻿from rest_framework import generics, permissions, filters
 from rest_framework_simplejwt.authentication import JWTAuthentication # <--- IMPORTANTE
 from django.utils import timezone
 from agendamento.models import Agendamento
@@ -9,11 +9,39 @@ from .serializers import PacienteSerializer
 class PacienteListCreateView(generics.ListCreateAPIView):
     queryset = Paciente.objects.all().order_by('-criado_em')
     serializer_class = PacienteSerializer
-    
-    # --- BLINDAGEM DE SEGURANÇA ---
-    authentication_classes = [JWTAuthentication] # Força aceitar o Token
+
+    # --- BLINDAGEM DE SEGURANCA ---
+    authentication_classes = [JWTAuthentication] # Forca aceitar o Token
     permission_classes = [permissions.IsAuthenticated]
 
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome', 'cpf', 'telefone', 'cidade']
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        params = self.request.query_params
+
+        cpf = params.get('cpf')
+        if cpf:
+            qs = qs.filter(cpf__icontains=cpf)
+
+        cidade = params.get('cidade')
+        if cidade:
+            qs = qs.filter(cidade__icontains=cidade)
+
+        prioridade = params.get('prioridade')
+        if prioridade:
+            qs = qs.filter(prioridade=prioridade)
+
+        sexo = params.get('sexo')
+        if sexo:
+            qs = qs.filter(sexo=sexo)
+
+        telefone = params.get('telefone')
+        if telefone:
+            qs = qs.filter(telefone__icontains=telefone)
+
+        return qs
 
 class PacienteAtendimentoListView(generics.ListAPIView):
     serializer_class = AgendamentoSerializer
@@ -61,40 +89,11 @@ class PacienteAtendimentoListView(generics.ListAPIView):
             qs = qs.filter(triagem__isnull=False)
 
         return qs.order_by('horario')
-    
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['nome', 'cpf', 'telefone', 'cidade']
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        params = self.request.query_params
-
-        cpf = params.get('cpf')
-        if cpf:
-            qs = qs.filter(cpf__icontains=cpf)
-
-        cidade = params.get('cidade')
-        if cidade:
-            qs = qs.filter(cidade__icontains=cidade)
-
-        prioridade = params.get('prioridade')
-        if prioridade:
-            qs = qs.filter(prioridade=prioridade)
-
-        sexo = params.get('sexo')
-        if sexo:
-            qs = qs.filter(sexo=sexo)
-
-        telefone = params.get('telefone')
-        if telefone:
-            qs = qs.filter(telefone__icontains=telefone)
-
-        return qs
 
 class PacienteDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
-    
-    # --- BLINDAGEM DE SEGURANÇA ---
-    authentication_classes = [JWTAuthentication] # Força aceitar o Token
+
+    # --- BLINDAGEM DE SEGURANCA ---
+    authentication_classes = [JWTAuthentication] # Forca aceitar o Token
     permission_classes = [permissions.IsAuthenticated]
